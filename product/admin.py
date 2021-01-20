3e402f643384eabdb125827307ad87f6458efb8d
Imaralievi4 <zkd3year@gmail.com>
2021-01-20 21:11:50 +0600

    initial commit

---
+++ product/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+from .models import Category, Product, ProductImage
+
+
+class ImageInLine(admin.TabularInline):
+    model = ProductImage
+    extra = 3
+    fields = ['image']
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        ImageInLine
+    ]
+    list_display = ('uuid', 'title', 'price')
+    list_display_links = ('uuid', 'title')
+
+
+admin.site.register(Category)
+admin.site.register(Product, ProductAdmin)
+# admin.site.register(ProductImage)
